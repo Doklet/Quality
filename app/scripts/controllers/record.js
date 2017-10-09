14e2418a8c455d3f102d1d021a65d4d11c367947
Enouk <tandy.nilsson@gmail.com>
2017-10-09 06:43:49 +0200

    More basic ui

--- app/scripts/controllers/record.js
+++ app/scripts/controllers/record.js
@@ -1,10 +1,17 @@
 'use strict';
 
 angular.module('qualityApp')
-  .controller('RecordCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('RecordCtrl', function($scope) {
+  	
+    $scope.labels = [{
+      id: 0,
+      name: 'background'
+    }, {
+      id: 1,
+      name: 'ok'
+    }, {
+      id: 2,
+      name: 'not ok'
+    }];
+
   });
